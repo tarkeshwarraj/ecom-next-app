2a09ed0c9b25ffb38a56a26e6c22a7515d57c5b8
tarkeshwar raj <tarkeshwarraj@gmail.com>
2025-04-04 09:30:59 +0530

    admin

---
+++ app/admin/layout.jsx
@@ -0,0 +1,21 @@
+import { assets } from "@/Assets/assets";
+import Sidebar from "@/Components/AdminComponents/Sidebar";
+import Image from "next/image";
+
+export default function layout({ children }) {
+  return (
+    <>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex flex-col w-full">
+          <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border-b border-black">
+            <h3 className="font-medium">Admin Pannel</h3>
+            <Image src={assets.profile_icon} alt="" width={40} />
+          </div>
+
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ app/api/blog/route.js
@@ -0,0 +1,52 @@
+import ConnectDB from "@/lib/config/db"
+import { NextResponse } from "next/server";
+import {writeFile} from 'fs/promises'
+import BlogModel from "@/lib/models/BlogModel";
+
+
+
+const LoadDB = async()=>{
+    await ConnectDB();
+}
+
+LoadDB();
+
+export async function GET(request){
+    
+    console.log("Blog GET HIT")
+    return NextResponse.json({
+        msg:"API Working"
+    })
+}
+
+export async function POST(request){
+
+    const formData = await request.formData();
+    const timestamp = Date.now();
+    const image = formData.get('image');
+    const imageByteData = await image.arrayBuffer();
+    const buffer = Buffer.from(imageByteData);
+    const path = `./public/${timestamp}_${image.name}`;
+    await writeFile(path, buffer);
+    const imgUrl = `/${timestamp}_${image.name}`;
+    
+    const blogData = {
+        title: `${formData.get('title')}`,
+        description: `${formData.get('description')}`,
+        category: `${formData.get('category')}`,
+        author: `${formData.get('author')}`,
+        image: `${imgUrl}`,
+        authorImg: `${formData.get('authorImg')}`
+    }
+
+    await BlogModel.create(blogData);
+
+    console.log("Blog Saved");
+
+    return NextResponse.json({
+        success:true,
+        msg:"Blog Added"
+    })
+}
+
+
